bec630775e05ffd78a281c97bd2f555d852c3896
Дмитро Миронов <hattabith@gmail.com>
2025-12-23 22:26:22 +0200

    Add device connections setup

--- TensileTestingApp/ViewModel/MainWindowViewModel.cs
+++ TensileTestingApp/ViewModel/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using OxyPlot;
 using OxyPlot.Series;
 using System.ComponentModel;
+using System.IO.Ports;
 using System.Windows.Controls;
 
 namespace TensileTestingApp.ViewModel
@@ -29,14 +30,24 @@ namespace TensileTestingApp.ViewModel
 
         public MainWindowViewModel()
         {
-            this.MyModel = new PlotModel { Title = "Tensile Test Data" };
-            this.MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
-
+            this.ForcePlot = new PlotModel { Title = "Force Data" };
+            this.ForcePlot.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+            this.LengthPlot = new PlotModel { Title = "Length Data" };
+            this.LengthPlot.Series.Add(new FunctionSeries(Math.Tan, 0, 10, 0.1, "tan(x)"));
+            this.ForceLengthPlot = new PlotModel { Title = "Force/Length Data" };
+            this.ForceLengthPlot.Series.Add(new FunctionSeries(Math.Exp, 0, 10, 0.1, "exp(x)"));
+            this.PortList = SerialPort.GetPortNames().ToList<string>();
+            this.RS485Address = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
         }
         public event PropertyChangedEventHandler? PropertyChanged;
 
 
-        public PlotModel MyModel { get; private set; }
+        public PlotModel ForcePlot { get; private set; }
+        public PlotModel LengthPlot { get; private set; }
+        public PlotModel ForceLengthPlot { get; private set; }
+        public List<string> PortList { get; set; }
+        public List<int> RS485Address { get; set; }
+
 
     }
 }
