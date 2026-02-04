46ae820a0ec469067ede91d2af78b9cd94927eb6
Дмитро Миронов <hattabith@gmail.com>
2026-02-04 12:04:42 +0200

    Add part of async com port methods

--- TensileTestingApp/Views/MainPage.xaml.cs
+++ TensileTestingApp/Views/MainPage.xaml.cs
@@ -16,51 +16,87 @@ namespace TensileTestingApp.Views
             InitializeComponent();
         }
 
-        private void ConnectButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        private async void ConnectButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            sp = new SerialPortCommunications(COMPortComboBox.SelectedItem.ToString(), (Int32)BaudRateComboBox.SelectedItem, Address485ComboBox.SelectedIndex);
 
 
             // треба зробити поток для ініціалізації з'єднання, після того як ініціалізація успішно відбулася,
             // тоді робимо поток читання і виводу в текстове поле
             // треба розібратися з змінною, яка відповідає за створення екземпляру класу SerialPortCommunications
-            if (!sp.IsOpen)
-            {
-                // TODO: Need refactoring
 
-
-                //sp = new SerialPortCommunications(COMPortComboBox.SelectedItem.ToString(), (Int32)BaudRateComboBox.SelectedItem, Address485ComboBox.SelectedIndex);
-                sp.OpenConnection();
-                // Чому не міняє значення одразу?
-                ConnectButton.Content = "Disconnect";
-                COMPortComboBox.IsEnabled = false;
-                BaudRateComboBox.IsEnabled = false;
-                Address485ComboBox.IsEnabled = false;
-                ConnectButton.IsEnabled = false;
-                OutputTextBlock.Text += "Connected to " + sp.GetPortName() + " at " + sp.GetBaudRate().ToString() + " baud, address " + sp.GetDeviceAddress().ToString() + "";
-                Task.Delay(1000);
-                ConnectButton.IsEnabled = true;
-                OutputTextBlock.Text += "Sending: #02";
-                sp.WriteToPort("#02", 300);
-                OutputTextBlock.Text += "Response is: " + sp.ReadFromPort();
-            }
-            else
+            try
             {
-                //sp = new SerialPortCommunications(COMPortComboBox.SelectedItem.ToString(), (Int32)BaudRateComboBox.SelectedItem, Address485ComboBox.SelectedIndex);
-                sp.CloseConnection();
-                ConnectButton.Content = "Connect";
-                COMPortComboBox.IsEnabled = true;
-                BaudRateComboBox.IsEnabled = true;
-                Address485ComboBox.IsEnabled = true;
-                ConnectButton.IsEnabled = true;
-                Task.Delay(1000);
+                if (_connectionState == ConnectionState.Disconnected)
+                {
+                    await ConnectAsync();
+                }
+                else if (_connectionState == ConnectionState.Connected)
+                {
+                    await DisconnectAsync();
+                }
             }
-            //var sp = new SerialPortCommunications(COMPortComboBox.SelectedItem.ToString(), (Int32)BaudRateComboBox.SelectedItem, Address485ComboBox.SelectedIndex);
-            //sp.OpenConnection();
-            //ConnectButton.Content = "Disconnect";
-            //ConnectButton.IsEnabled = false;
-            //await Task.Delay(1000);
-            //ConnectButton.IsEnabled = true;
+            catch (Exception ex)
+            {
+                OutputTextBlock.Text += $"Error: {ex.Message}\n";
+                _connectionState = ConnectionState.Error;
+                UpdateUiState();
+            }
+
         }
+        private async Task ConnectAsync()
+        {
+            _connectionState = ConnectionState.Connecting;
+            UpdateUiState();
+
+            _sp = new SerialPortCommunications(
+                COMPortComboBox.SelectedItem.ToString(),
+                (int)BaudRateComboBox.SelectedItem,
+                Address485ComboBox.SelectedIndex);
+
+            await Task.Run(() => _sp.OpenConnection());
+
+            _connectionState = ConnectionState.Initializing;
+            UpdateUiState();
+
+        }
+        private async Task DisconnectAsync()
+        {
+            _connectionState = ConnectionState.Disconnecting;
+            UpdateUiState();
+
+            await Task.Run(() => _sp.CloseConnection());
+            _sp = null;
+
+            _connectionState = ConnectionState.Disconnected;
+            UpdateUiState();
+        }
+        private void UpdateUiState()
+        {
+            switch (_connectionState)
+            {
+                case ConnectionState.Disconnected:
+                    ConnectButton.Content = "Connect";
+                    ConnectButton.IsEnabled = true;
+                    COMPortComboBox.IsEnabled = true;
+                    BaudRateComboBox.IsEnabled = true;
+                    Address485ComboBox.IsEnabled = true;
+                    break;
+
+                case ConnectionState.Connecting:
+                case ConnectionState.Initializing:
+                    ConnectButton.Content = "Connecting...";
+                    ConnectButton.IsEnabled = false;
+                    COMPortComboBox.IsEnabled = false;
+                    BaudRateComboBox.IsEnabled = false;
+                    Address485ComboBox.IsEnabled = false;
+                    break;
+
+                case ConnectionState.Connected:
+                    ConnectButton.Content = "Disconnect";
+                    ConnectButton.IsEnabled = true;
+                    break;
+            }
+        }
+
     }
 }
