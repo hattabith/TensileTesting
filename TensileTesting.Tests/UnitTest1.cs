0bb817521244e9c4b37aa552b4d43d2e53fa770c
Дмитро Миронов <hattabith@gmail.com>
2026-02-12 00:32:45 +0200

    Add xUnit Test proj

---
+++ TensileTesting.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace TensileTesting.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+        [Theory]
+        [InlineData("-03.700", -3.7)]
+        [InlineData("+03.700", 3.7)]
+        public void TestADCDataParser(string data, double expected)
+        {
+            var parser = new TensileTestingApp.Models.ADCDataParser();
+            var result = parser.ParseWithOutCheckSum(data);
+            Assert.NotNull(result);
+        }
+    }
+}
